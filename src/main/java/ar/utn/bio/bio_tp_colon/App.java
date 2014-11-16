a01444f60f70ff9308d78445f58934d3026204f0
ruben.fanego <ruben.fanego@mercadolibre.com>
2014-11-15 21:51:58 -0300

    writing protein sequence in fasta file

--- src/main/java/ar/utn/bio/bio_tp_colon/App.java
+++ src/main/java/ar/utn/bio/bio_tp_colon/App.java
@@ -1,21 +1,31 @@
 package ar.utn.bio.bio_tp_colon;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 
 import org.biojava3.core.sequence.DNASequence;
+import org.biojava3.core.sequence.ProteinSequence;
 import org.biojava3.core.sequence.io.FastaWriterHelper;
 import org.biojava3.core.sequence.io.GenbankReaderHelper;
+import org.biojava3.core.sequence.transcription.Frame;
 
 public class App{
     public static void main( String[] args ) throws Exception{
     	File dnaFile = new File("src/main/resources/sequence.gb");		
      
     	LinkedHashMap<String, DNASequence> dnaSequences = GenbankReaderHelper.readGenbankDNASequence( dnaFile );
+    	List<ProteinSequence> proteinSequences = new ArrayList<ProteinSequence>();
     	for (DNASequence sequence : dnaSequences.values()) {
+    			for(Frame frame : Frame.values()){    				
+    				ProteinSequence proteinSequence = sequence.getRNASequence(frame).getProteinSequence();
+    				proteinSequences.add(proteinSequence);
+    			}
     	    	System.out.println( sequence.getSequenceAsString() );
     	}
+    	FastaWriterHelper.writeProteinSequence(new File("src/main/resources/fasta-protein.fna"), proteinSequences);
     	
-    	FastaWriterHelper.writeNucleotideSequence(new File("src/main/resources/fasta-colon.fna"),dnaSequences.values());
+    	FastaWriterHelper.writeNucleotideSequence(new File("src/main/resources/fasta-dna.fna"),dnaSequences.values());
     }
 }
